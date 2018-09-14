cd7e943d5cbddcb085caf76c897d95a19ff039a3
Koy Saeteurn <kosatun@aol.com>
2018-09-14 06:23:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,25 +50,45 @@ class Student
     end  
   end   
   
-  def self.first_X_students_in_grade_10(n)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 10 LIMIT n
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT ?
     SQL
-    
-    DB[:conn].execute(sql).map do |row|
+
+    DB[:conn].execute(sql, number).map do |row|
       self.new_from_db(row)
-    end 
-  end   
-  
+    end
+  end
+
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 10 
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id LIMIT 1
     SQL
-    
+
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end 
-  end   
+    end.first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      ORDER BY students.id
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end 
   
   def save
     sql = <<-SQL
