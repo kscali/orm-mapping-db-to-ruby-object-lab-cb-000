333ce6442708b4f35f1bf0c07b74947c93d6a523
Koy Saeteurn <kosatun@aol.com>
2018-09-14 05:57:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,6 +27,25 @@ class Student
       self.new_from_db(row)
   end
   
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM WHERE grade = 9 
+    SQL
+    
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    
+  end   
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+  end   
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
