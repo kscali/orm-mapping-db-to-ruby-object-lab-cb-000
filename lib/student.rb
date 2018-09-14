3e3d3082e8b4f7994ac12fa2e19508b082872db1
Koy Saeteurn <kosatun@aol.com>
2018-09-14 06:10:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,9 +35,7 @@ class Student
       SELECT * FROM students WHERE grade = 9 
     SQL
     
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end 
+    
   end   
   
   def self.students_below_12th_grade
